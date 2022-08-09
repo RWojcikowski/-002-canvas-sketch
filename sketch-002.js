42adce58f62af350982153a1abb7e1485ee4c6aa
RWojcikowski <radoslaw.chicqen@gmail.com>
2022-08-09 23:22:28 +0200

    implement logic point +  interact using mouse

--- sketch-002.js
+++ sketch-002.js
@@ -1,16 +1,24 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [ 1080, 1080 ]
+  dimensions: [ 1080, 1080 ],
+  animate : true,
 };
 
-const sketch = () => {
+let elCanvas;
+let points
 
-const points =[
+const sketch = ({ canvas }) => {
+
+points =[
   new Point({x:200, y: 540 }),
   new Point({x:500, y: 100, control: true}),
   new Point({x:880, y: 540 }),
-]
+];
+
+canvas.addEventListener('mousedown', onMouseDown);
+
+elCanvas = canvas;
 
   return ({ context, width, height }) => {
     context.fillStyle = 'white';
@@ -18,7 +26,7 @@ const points =[
 
     context.beginPath();
     context.moveTo(points[0].x, points[0].y);
-    context.quadraticCurveTo(points[1].x, points[1].y,points[2].x, points[2].y);
+    context.quadraticCurveTo(points[1].x, points[1].y, points[2].x, points[2].y);
     context.stroke();
 
     points.forEach(point =>{
@@ -27,7 +35,38 @@ const points =[
   };
 };
 
+const onMouseDown= (e) =>{
+  window.addEventListener('mousemove', onMouseMove)
+  window.addEventListener('mouseup', onMouseUp)
+
+const x = (e.offsetX / elCanvas.offsetWidth) * elCanvas.width;
+const y = (e.offsetY / elCanvas.offsetHeight) * elCanvas.height;
+
+points.forEach(point => {
+  point.isDragging = point.hitTest(x, y);
+});
+};
+
+const onMouseMove = (e) => {
+const x = (e.offsetX / elCanvas.offsetWidth) * elCanvas.width;
+const y = (e.offsetY / elCanvas.offsetHeight) * elCanvas.height;
+
+points.forEach(point =>{
+  if(point.isDragging){
+    point.x = x;
+    point.y = y;
+  }
+});
+
+};
+
+const onMouseUp = () => {
+  window.removeEventListener('mousemove', onMouseMove)
+  window.removeEventListener('mouseup', onMouseUp)
+};
 canvasSketch(sketch, settings);
+
+
 
 class Point {
   constructor({x, y, control = false}){
@@ -46,4 +85,12 @@ class Point {
     context.fill();
  context.restore();
   }
+hitTest(x, y){
+  const dx = this.x - x;
+  const dy = this.y - y; 
+  const dd = Math.sqrt(dx * dx + dy * dy);
+
+  return dd < 20;
+}
+
 }
